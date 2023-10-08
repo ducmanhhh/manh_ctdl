0e7d38c11d490a861a6871252839ac3c16e8f058
BuiDucManh_CTDL_ <145543436+ducmanhhh@users.noreply.github.com>
2023-10-08 20:58:12 +0700

    Add files via upload

---
+++ interview01.py
@@ -0,0 +1,18 @@
+def reverse(nums):
+
+    start_index=0
+
+    end_index=len(nums)-1
+
+    while end_index > start_index:
+
+        nums[start_index],nums[end_index]=nums[end_index],nums[start_index]
+        start_index=start_index + 1
+        end_index = end_index - 1
+
+
+if _name_ =='_main_':
+
+    n=[1,2,3,4]
+    reverse(n)
+    print(n)
---
+++ interview4.py
@@ -0,0 +1,14 @@
+def are_anagrams(word1, word2):
+    word1 = word1.replace(" ", "").lower()
+    word2 = word2.replace(" ", "").lower()
+
+    return sorted(word1) == sorted(word2)
+
+word1 = "ngu"
+word2 = "ung"
+result = are_anagrams(word1, word2)
+
+if result:
+    print(f'"{word1}" and "{word2}" are anagrams.')
+else:
+    print(f'"{word1}" and "{word2}" are not anagrams.')
